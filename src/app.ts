aa74fe91b45fe5c301043db899656d67e7ad168b
Rahimvi <rahimvi414@gmail.com>
2023-05-30 02:33:23 +0300

    [DONE] mongoose,express,cors,perser

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+//cors
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//tasting
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
