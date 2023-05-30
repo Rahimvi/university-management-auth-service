45fceab34f3bc58ed84a2c61195f7055c7f24234
Rahimvi <rahimvi414@gmail.com>
2023-05-31 00:22:56 +0300

    test

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+import express, { Application, Request } from 'express'
 
-const app: Application = express();
+const app: Application = express()
 //cors
-app.use(cors());
+app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 //tasting
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: any) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
