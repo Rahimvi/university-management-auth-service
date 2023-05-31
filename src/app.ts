ae4397496a3dacfa4676e14a25c43ace553d5022
Rahimvi <rahimvi414@gmail.com>
2023-05-31 03:27:34 +0300

    [DONE] Create User service controller and router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
-
+import usersRouter from './app/modules/users/users.router'
 const app: Application = express()
 //cors
 app.use(cors())
@@ -9,6 +9,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 //tasting
+//Application Routes
+app.use('/api/v1/users/', usersRouter)
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
---
+++ src/app/modules/users/users.utils.ts
@@ -0,0 +1,13 @@
+import { User } from './users.model'
+
+export const findLastUser = async () => {
+  const lastUser = await User.findOne({}, { id: 1, _id: 0 }).sort({
+    createdAt: -1,
+  })
+  return lastUser?.id
+}
+export const generateUserId = async () => {
+  const currentId = (await findLastUser()) || (0).toString().padStart(5, '0')
+  const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return incrementedId
+}
